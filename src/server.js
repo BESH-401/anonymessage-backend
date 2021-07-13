7fa269fa1a2be01588f049e58f83e3e429a7a24b
Keian <KeianAnthony1@gmail.com>
2021-07-13 14:54:30 -0700

    potential fix

--- src/server.js
+++ src/server.js
@@ -5,7 +5,10 @@ const { Server } = require('socket.io');
 const PORT = process.env.PORT || 3000;
 const server = new Server(PORT, {
   cors:
-    { origin: ['https://anony-message-backend.herokuapp.com/'], methods: ['GET'] }
+  {
+    origin: ['https://anony-message-backend.herokuapp.com'],
+    methods: ['GET']
+  }
 });
 
 module.exports = server;
